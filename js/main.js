ad44337b32f7edc0e24da6070a254fe3c792277a
Digital Villa <villanueva1077@gmail.com>
2017-12-27 17:29:12 -0700

    version 1.1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 var btn = document.querySelector('.btn');
 var priceSpan = document.querySelector('.currency');
+var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 var currency = "USD";
-xhRequest();
+var price;
+fetchRequest();
 
 btn.addEventListener("click", function () {
     xhRequest();
-
 });
 
+
+//Having fun with XHR
 function xhRequest() {
     var XHR = new XMLHttpRequest();
     XHR.onreadystatechange = function () {
         if (XHR.readyState === 4 && XHR.status == 200) {
-            //Pasre the link into a Json object and pull the image url 
-            var price = JSON.parse(XHR.responseText).bpi[currency].rate;
-            console.log(price);
+            price = JSON.parse(XHR.responseText).bpi[currency].rate;
             priceSpan.innerText = price +" "+ currency;
+            console.log(price);
         }
     };
-    //This is a 997 image list from unsplash.com
-    XHR.open("Get", "https://api.coindesk.com/v1/bpi/currentprice.json");
+    XHR.open("Get", url);
     XHR.send(null);
 }
+
+
+//Common Fetch Request 
+function fetchRequest () {
+    fetch(url).then(function(response){
+        return response.json().then(function(data){
+            price = data.bpi[currency].rate;
+            priceSpan.innerText = price +" "+ currency;
+           console.log(price);
+       });
+    });
+};
